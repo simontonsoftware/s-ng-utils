178e1e5da9f797b02cdc014c005c302e7bd49e30
Eric <eric@simontonsoftware.com>
2018-07-24 21:40:58 -0400

    chore: strict typescript, remove scaffolded components & such

--- projects/s-ng-utils/src/lib/s-ng-utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SNgUtilsComponent } from './s-ng-utils.component';
-
-@NgModule({
-  imports: [],
-  declarations: [SNgUtilsComponent],
-  exports: [SNgUtilsComponent]
-})
-export class SNgUtilsModule {}
--- projects/s-ng-utils/src/public_api.ts
+++ projects/s-ng-utils/src/public_api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of s-ng-utils
  */
-
-export * from './lib/s-ng-utils.service';
-export * from './lib/s-ng-utils.component';
-export * from './lib/s-ng-utils.module';
