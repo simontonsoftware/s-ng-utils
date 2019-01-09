b00f3903b3fae28749afe824c9777c5b190dd7b8
ersimont <8042088+ersimont@users.noreply.github.com>
2019-01-08 21:12:03 -0500

    fix: expose `DirectiveSuperclass` in the public api

--- projects/s-ng-utils/src/public_api.ts
+++ projects/s-ng-utils/src/public_api.ts
@@ -2,6 +2,12 @@
  * Public API Surface of s-ng-utils
  */
 
-export * from "./lib/auto-destroyable";
-export * from "./lib/form-control-superclass";
-export * from "./lib/wrapped-form-control-superclass";
+export { AutoDestroyable } from "./lib/auto-destroyable";
+export { DirectiveSuperclass } from "./lib/directive-superclass";
+export {
+  FormControlSuperclass,
+  provideValueAccessor,
+} from "./lib/form-control-superclass";
+export {
+  WrappedFormControlSuperclass,
+} from "./lib/wrapped-form-control-superclass";
