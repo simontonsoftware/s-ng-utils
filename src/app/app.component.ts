25488329a95ecb8dc29520e38718de17281412a3
ersimont <8042088+ersimont@users.noreply.github.com>
2019-01-08 21:11:34 -0500

    chore: add an integration test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, Injector } from "@angular/core";
+import {
+  AutoDestroyable,
+  DirectiveSuperclass,
+  FormControlSuperclass,
+  provideValueAccessor,
+  WrappedFormControlSuperclass,
+} from "s-ng-utils";
 
 @Component({
   selector: "app-root",
@@ -6,5 +13,18 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "s-ng-utils-platform";
+  title = "failure";
+
+  constructor(injector: Injector) {
+    // use each function once just to show in can be imported
+    new class extends AutoDestroyable {}();
+    new class extends DirectiveSuperclass {}(injector);
+    provideValueAccessor(AppComponent);
+    new class extends FormControlSuperclass<string> {
+      handleIncomingValue(value: string) {}
+    }(injector);
+    new class extends WrappedFormControlSuperclass<string> {}(injector);
+
+    this.title = "s-ng-utils-platform";
+  }
 }
